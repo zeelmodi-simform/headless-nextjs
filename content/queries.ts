564b3ca837efd267395a9a74d77f453408865cb4
Zeel Modi <zeel.m@simformsolutions.com>
2024-09-05 11:31:59 +0530

    feat: Pre-rendering and dynamic routing

--- content/queries.ts
+++ content/queries.ts
@@ -1,5 +1,40 @@
-import { HeroQuery, LogoWallQuery } from "@/types";
+import 'server-only';
+
+import { CustomerPostQuery, HeaderNavQuery, HeroQuery, LogoWallQuery } from "@/types";
 import { contentGqlFetcher } from "./fetch";
+
+export const getContentForHeaderNav = async () => {
+    const query = `#graphql
+        query NavigationCollection($where: NavigationFilter) {
+            navigationCollection(where: $where) {
+                items {
+                name
+                linksCollection {
+                    items {
+                        label
+                        link
+                        }
+                    }
+                }
+            }
+        }
+    `;
+    
+    const data = await contentGqlFetcher<HeaderNavQuery>({
+        query, variables: {
+            "where": {
+                "name": "Header"
+            }
+        }
+    })
+
+    if (!data) {
+        throw Error('No data found');
+    }
+
+    return data;
+
+};
 
 export const getContentForHero = async () => {
     const query = `#graphql
@@ -52,4 +87,76 @@ export const getContentForLogoWall = async () => {
     }
 
     return data
+}
+
+export const getSlugsForPost = async () => {
+    const query = `#graphql
+        query SlugsForPost {
+            customerPostCollection {
+                items {
+                    slug
+                }
+            }
+        }
+    `;
+
+    const data = await contentGqlFetcher<{
+        customerPostCollection: {
+                items: {
+                    slug: string
+                }[]
+            }
+    }>({
+        query
+    });
+
+    if (!data) {
+        throw Error('No data found');
+    }
+
+    return data
+}
+
+export const getContentForCustomerPost = async (slug: string) => {
+    const query = `#graphql
+        query CustomerPostCollection($where: CustomerPostFilter) {
+            customerPostCollection(where: $where) {
+                items {
+                _id
+                title
+                slug
+                body {
+                    json
+                }
+                customer {
+                    name
+                    logo {
+                    url
+                    fileName
+                    title
+                    height
+                    width
+                    }
+                }
+                }
+            }
+        }
+    `;
+
+
+    const data = await contentGqlFetcher<CustomerPostQuery>({
+        query, variables: {
+            "where": {
+                // "slug": "how-canon-leverages-stellar-x-to-onboard-new-hosts"
+                slug
+            }
+        }
+    });
+
+    if (!data) {
+        throw Error('No data found');
+    }
+
+    return data
+
 }
